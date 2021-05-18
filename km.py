4a1041dd7d19a77322e2c7a0d61820c25195a8d3
ROSHAN JOE BENNY <75135163+roshanjoebenny@users.noreply.github.com>
2021-05-18 11:10:32 +0530

    This is a program to convert distance given in kilometer into meter and meters into kilometers

---
+++ km.py
@@ -0,0 +1,6 @@
+a=int(input('enter the value of distance in km='))
+b=a*1000
+print('the value of converted distance in meter= ',b)
+c=int(input('enter the value of distance in meter='))
+d=c/1000
+print('the value of converted distance in km=',d)
